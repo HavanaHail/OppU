7fa4d02d7765749602c883d3cd02677797865de6
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-26 12:13:48 -0400

    double age functions and close button working @12:13

--- static/PageTwo.js
+++ static/PageTwo.js
@@ -1,4 +1,25 @@
+let addyearcounter = 0;
+
 function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
+
+}
+
+function incrementAge() {
+  addyearcounter = addyearcounter+1
+  console.log(addyearcounter)
+  if (addyearcounter % 2==0) {
+    console.log ("add year counter")
+    var popup = document.getElementById("popup1");
+    popup.style.visibility="visible";
+    popup.style.opacity="1";
+  }
+};
+
+function closePopUp() {
+  var popup = document.getElementById("popup1")
+  popup.style.visibility="hidden";
+  popup.style.opacity="0";
+
 }
